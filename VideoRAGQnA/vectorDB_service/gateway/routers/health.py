0756f45d48e66d7f6da06a330048579ea9f4803d
BaoHuiling <huiling.bao@intel.com>
2024-06-05 16:41:37 +0800

    add vectordb as file

---
+++ VideoRAGQnA/vectorDB_service/gateway/routers/health.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+from fastapi import APIRouter, Header
+# from conf.config import Settings
+# from core.common.auth import securedRoute
+
+router = APIRouter()
+# settings = Settings()
+
+
+@router.get("/health", tags=["Health API"], summary="Check API health")
+@router.get("/health/", include_in_schema=False)
+# @securedRoute
+# async def health(authorization: str = Header(default=None)):
+async def health():
+    """
+
+    **Response**:
+
+    - **status** (string): A string describing health status.
+    """
+    return {"status": "healthy"}
